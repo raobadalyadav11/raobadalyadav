80b94b0c61f3cafd3c56995391ed9732f7dfa417
Badal Kumar <bkbadal7779810465@gmail.com>
2025-08-12 19:14:02 +0530

    remove

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://raobadalyadav11:Bkbadal33@clustercode.f5rduf0.mongodb.net/portfolio?retryWrites=true&w=majority';
+const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable');
